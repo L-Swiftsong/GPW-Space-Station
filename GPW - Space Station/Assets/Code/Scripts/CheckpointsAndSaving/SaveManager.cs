91fcd676b366a435c9062ccdaedf585af79f6a78
L-Swiftsong <acarey300@caledonian.ac.uk>
2024-11-08 21:29:06 +0000

    Saving to & Loading from files start
    We have made a start on Saving to/Loading from .json files, with separate naming conventions for Autosaves and Manual saves, the ability to select the most recently created file, and within that the most recently saved element (Manual, Checkpoint, Hub, etc).
    
    Currently these are called via the inspector and aren't hooked up to the actual saving, and there seems to be an issue where the flashlight battery doesn't update.

--- GPW - Space Station/Assets/Code/Scripts/CheckpointsAndSaving/SaveManager.cs
+++ GPW - Space Station/Assets/Code/Scripts/CheckpointsAndSaving/SaveManager.cs
@@ -2,23 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SceneManagement;
+using System.IO;
 
 namespace Saving
 {
     public class SaveManager : MonoBehaviour
     {
-        /// <summary> Replace this with something like PlayerPrefs or a .json file, as this doesn't actually save between sessions.</summary>
         [System.Serializable]
-        private struct TempSaveData
+        private struct SaveData
         {
+            public float SaveTime;
+
             public PlayerManager.PlayerSetupData PlayerData;
 
-            public static TempSaveData Default = new TempSaveData() {
+            public static SaveData Default = new SaveData() {
                 PlayerData = PlayerManager.PlayerSetupData.Default,
+                SaveTime = 0.0f,
             };
         }
-        private static TempSaveData s_checkpointSaveData = TempSaveData.Default;
-        private static TempSaveData s_hubEnterSaveData = TempSaveData.Default;
+        private static SaveData s_checkpointSaveData = SaveData.Default;
+        private static SaveData s_hubEnterSaveData = SaveData.Default;
+        private static SaveData s_manualSaveData = SaveData.Default;
+
+
+        [System.Serializable]
+        private struct SaveDataBundle
+        {
+            public SaveData CheckpointSaveData;
+            public SaveData HubSaveData;
+            public SaveData ManualSaveData;
+        }
+        private SaveDataBundle GetSaveDataBundle() => new SaveDataBundle() {
+            CheckpointSaveData = s_checkpointSaveData,
+            HubSaveData = s_hubEnterSaveData,
+            ManualSaveData = s_manualSaveData,
+        };
+        private void OverrideFromSaveDataBundle(SaveDataBundle bundle)
+        {
+            s_checkpointSaveData = bundle.CheckpointSaveData;
+            s_hubEnterSaveData = bundle.HubSaveData;
+            s_manualSaveData = bundle.ManualSaveData;
+        }
+
 
 
         private void OnEnable()
@@ -49,12 +74,15 @@ namespace Saving
 
         public static void SaveCheckpoint() => SaveGameState(ref s_checkpointSaveData);
         public static void SaveHub() => SaveGameState(ref s_hubEnterSaveData);
+        public static void ManualSave() => SaveGameState(ref s_manualSaveData);
 
 
-        private static void SaveGameState(ref TempSaveData saveData)
+        private static void SaveGameState(ref SaveData saveData)
         {
             // Clear current save data.
-            saveData = new TempSaveData();
+            saveData = new SaveData();
+            saveData.SaveTime = Time.time;
+
 
             // Save Player Data.
             saveData.PlayerData = PlayerManager.Instance.GetCurrentPlayerData();
@@ -67,12 +95,92 @@ namespace Saving
 
             #endregion
         }
-        private static void LoadGameState(ref TempSaveData saveData)
+        private static void LoadGameState(ref SaveData saveData)
         {
             // Load Player Data.
             PlayerManager.Instance.LoadFromPlayerData(saveData.PlayerData);
 
             // Load Level Data.
+        }
+
+
+        [ContextMenu("Manual Save")]
+        private void SaveIntoJSON()
+        {
+            string saveDataBundle = JsonUtility.ToJson(GetSaveDataBundle(), true);
+
+            string slashlessTime = System.DateTime.Now.ToString().Replace('/', '_').Replace(':', '_');
+            string fileName = "ManualSaveData (" + slashlessTime + ").json";
+
+            File.WriteAllText(Application.persistentDataPath + "/" + fileName, saveDataBundle);
+        }
+        [ContextMenu("Autosave")]
+        private void SaveAutosaveToJSON()
+        {
+            string saveDataBundle = JsonUtility.ToJson(GetSaveDataBundle(), true);
+
+            string fileName = "Autosave_0.json";
+
+            File.WriteAllText(Application.persistentDataPath + "/" + fileName, saveDataBundle);
+        }
+
+
+
+        [ContextMenu("Manual Load")]
+        private void LoadFromJSON()
+        {
+            string path = Application.persistentDataPath + "/";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] fileInfoArray = directoryInfo.GetFiles("ManualSaveData*.json");
+
+            if (fileInfoArray.Length <= 0)
+            {
+                Debug.LogError("ERROR: No save data to read.");
+            }
+
+            // Find the most recently saved file and retreive its data.
+            int mostRecentSaveIndex = 0;
+            Debug.Log("Initial File: '" + fileInfoArray[mostRecentSaveIndex].Name + "'. Created: " + fileInfoArray[mostRecentSaveIndex].CreationTimeUtc);
+            
+            for (int i = 1; i < fileInfoArray.Length; i++)
+            {
+                Debug.Log("Check File: '" + fileInfoArray[i].Name + "'. Created: " + fileInfoArray[i].CreationTimeUtc);
+                if (fileInfoArray[i].CreationTimeUtc > fileInfoArray[mostRecentSaveIndex].CreationTimeUtc)
+                {
+                    mostRecentSaveIndex = i;
+                }
+            }
+            Debug.Log("Most Recent File: '" + fileInfoArray[mostRecentSaveIndex].Name + "'. Created: " + fileInfoArray[mostRecentSaveIndex].CreationTimeUtc);
+
+
+            // Load from the file.
+            string saveDataText = File.ReadAllText(fileInfoArray[mostRecentSaveIndex].FullName);
+            SaveDataBundle saveDataBundle = JsonUtility.FromJson<SaveDataBundle>(saveDataText);
+            OverrideFromSaveDataBundle(saveDataBundle);
+
+            // Load the most recently saved item in the loaded bundle.
+            if (s_checkpointSaveData.SaveTime >= s_hubEnterSaveData.SaveTime)
+            {
+                if (s_manualSaveData.SaveTime >= s_checkpointSaveData.SaveTime)
+                {
+                    LoadGameState(ref s_manualSaveData);
+                }
+                else
+                {
+                    LoadGameState(ref s_checkpointSaveData);
+                }
+            }
+            else
+            {
+                if (s_manualSaveData.SaveTime >= s_hubEnterSaveData.SaveTime)
+                {
+                    LoadGameState(ref s_manualSaveData);
+                }
+                else
+                {
+                    LoadGameState(ref s_hubEnterSaveData);
+                }
+            }
         }
     }
 }
